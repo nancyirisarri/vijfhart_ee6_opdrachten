59be39f957d975872a37e65fd1771301678ec5e2
Nancy Irisarri <nancyirisarri@gmail.com>
2017-04-09 21:06:29 +0200

    Remove comments.

--- opdracht1-2-ServletProject/src/servlet/HelloWorld.java
+++ opdracht1-2-ServletProject/src/servlet/HelloWorld.java
@@ -2,32 +2,19 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloWorld
- */
 @WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-//    /**
-//     * @see HttpServlet#HttpServlet()
-//     */
-//    public HelloWorld() {
-//        super();
-//        // TODO Auto-generated constructor stub
-//    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, 
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
@@ -36,12 +23,4 @@ public class HelloWorld extends HttpServlet {
 		out.println("</body>");
 		out.println("</html>");
 	}
-
-//	/**
-//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//	}
-
 }
